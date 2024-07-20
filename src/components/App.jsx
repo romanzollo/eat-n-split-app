c38dd8d9a22b6050c4e1a3ce576e92f11f9f4657
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-07-20 20:51:27 +0200

    displaying the New Friend Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import FormAddFriend from './FormAddFriend';
 import FriendsList from './FriendsList';
 import Button from './Button';
 import FormSplitBill from './FormSplitBill';
 function App() {
+    const [showAddFriend, setShowAddFriend] = useState(false);
+
+    function handleShowAddFriend() {
+        // используем callback для изменения состояния так как новое состояние
+        // зависет от текущего состояния (ОБЯЗАТЕЛЬНО!!!)
+        setShowAddFriend((show) => !show);
+    }
+
     return (
         <div className="app">
             <div className="sidebar">
                 <FriendsList />
-                <FormAddFriend />
-                <Button>Add friend</Button>
+                {showAddFriend && <FormAddFriend />}
+
+                <Button onClick={handleShowAddFriend}>
+                    {showAddFriend ? 'Close' : 'Add friend'}
+                </Button>
             </div>
 
             <FormSplitBill />
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,10 @@
-function Button({ children }) {
-    return <button className="button">{children}</button>;
+function Button({ children, onClick }) {
+    return (
+        // добавляем в компонент кнопки onClick чтобы можно было нажать на кнопку (ОБЯЗАТЕЛЬНО!!!)
+        <button className="button" onClick={onClick}>
+            {children}
+        </button>
+    );
 }
 
 export default Button;
